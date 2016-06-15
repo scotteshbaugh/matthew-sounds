b95a24b1914704edec7dc0692e49d27478068749
Scott Eshbaugh <scotteshbaugh@us.ibm.com>
2016-06-15 16:58:29 -0500

    initial

---
+++ javascript/app.js
@@ -0,0 +1,29 @@
+// var audio1 = document.getElementById('sound1');
+// var audio2 = document.getElementById('sound2');
+// function playSound1(){
+// if (audio1.paused !== true){
+//     audio1.pause();
+//     audio2.play();
+//     }
+// else{
+//     audio2.play();
+//     }
+// }
+// function playSound2(){
+// if (audio2.paused !== true){
+//     audio2.pause();
+//     audio1.play();
+//     }
+// else{
+//     audio1.play();
+//     }
+// }
+$(document).on('ready', function() {
+    $('img').click(function(){
+        $(this).hide();
+    })
+
+    $('button').on('click', function() {
+
+    });
+});
